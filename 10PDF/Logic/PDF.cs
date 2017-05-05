4747ac2298cbc0f945122ffd2c2159b5fdd03620
dxs <glaglasven@live.com>
2017-05-05 14:05:23 +0200

    First page rendering OK

---
+++ 10PDF/Logic/PDF.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Pdf;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace _10PDF.Logic
+{
+	public class PDF : INotifyPropertyChanged
+	{
+
+		private PdfDocument pdfDocument;
+		private BitmapImage imgSource = null;
+		public BitmapImage ImgSource
+		{
+			get { return imgSource; }
+			set { imgSource = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ImgSource))); }
+		}
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public PDF()
+		{
+
+		}
+
+		public async void LoadFromFile()
+		{
+			pdfDocument = null;
+			ImgSource = null;
+			var picker = new Windows.Storage.Pickers.FileOpenPicker();
+			picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+			picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+			picker.FileTypeFilter.Add(".pdf");
+			Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+			if (file != null)
+			{
+				try
+				{
+					pdfDocument = await PdfDocument.LoadFromFileAsync(file);
+				}
+				catch (Exception ex)
+				{
+					Debug.WriteLine(ex.Message);
+					return;
+				}
+				if (pdfDocument != null)
+				{
+					uint page = pdfDocument.PageCount;
+					using (PdfPage currentPage = pdfDocument.GetPage(0))
+					{
+						var stream = new InMemoryRandomAccessStream();
+						await currentPage.RenderToStreamAsync(stream);
+						ImgSource = new BitmapImage();
+						await ImgSource.SetSourceAsync(stream);
+					}
+				}
+			}
+			else
+				return;
+		}
+	}
+}
---
+++ 10PDF/Pages/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using _10PDF.Logic;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Data.Pdf;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Composition;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace _10PDF
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+		
+		public PDF PDF = new PDF();
+
+		public MainPage()
+        {
+            this.InitializeComponent();
+			TransparentBar();
+			applyAcrylicAccent(MainGrid);
+        }
+
+		#region BackDrop
+		private void TransparentBar()
+		{
+			ApplicationViewTitleBar formattableTitleBar = ApplicationView.GetForCurrentView().TitleBar;
+			formattableTitleBar.ButtonBackgroundColor = Colors.Transparent;
+			CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+			coreTitleBar.ExtendViewIntoTitleBar = true;
+		}
+
+		private void applyAcrylicAccent(Panel e)
+		{
+			_compositor = ElementCompositionPreview.GetElementVisual(this).Compositor;
+			_hostSprite = _compositor.CreateSpriteVisual();
+			_hostSprite.Size = new Vector2((float)MainGrid.ActualWidth, (float)MainGrid.ActualHeight);
+
+			ElementCompositionPreview.SetElementChildVisual(
+					MainGrid, _hostSprite);
+			_hostSprite.Brush = _compositor.CreateHostBackdropBrush();
+		}
+		Compositor _compositor;
+		SpriteVisual _hostSprite;
+
+		private void MainGrid_SizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			if (_hostSprite != null)
+				_hostSprite.Size = e.NewSize.ToVector2();
+		}
+
+		#endregion
+
+		private void AppBarButton_Click(object sender, RoutedEventArgs e)
+		{
+			PDF.LoadFromFile();
+		}
+	}
+}
